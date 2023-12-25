f4ec6f04f9c69c954163fcad816c0ea4315ac2c7
kimwoojin <kimwoojin1903@gmail.com>
2023-12-26 00:18:12 +0900

    29번

--- jesus.js
+++ jesus.js
@@ -53,7 +53,8 @@ class ForExam{
     this.problem25 = `<nav>${this.problem25.join('')}</nav>`;
     this.problem26 = `<article>${this.problem26.map((tag) => `<section>${tag}</section>`).join('')}</article>`;
     this.problem27 = this.problem27.map((tag) => `<ol>${tag}</ol>`);
-    this.problem28 = this.problem28.map((tag) => `<input placeholder=${tag}>`)
+    this.problem28 = this.problem28.map((tag) => `<input placeholder=${tag}>`);
+    this.problem29 = `<footer>${this.problem29.join('')}</footer>`
   }
 
   generateProblemData(problemNumber, baseArray, specialProblems){
